cab278729d55c0fa8985a35427ea5945d2691d42
thechaos16 <thechaos16@gmail.com>
2023-06-20 23:42:10 +0900

    update get with parameter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use rocket::form::Form;
+
 #[macro_use] extern crate rocket;
+
+#[derive(FromForm)]
+struct AddInput {
+    a: i32,
+    b: i32
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/print/<string>")]
+fn print_val(string: String) -> String {
+    format!("Hello, {}!", string.as_str())
+}
+
+#[post("/add", data = "<input>")]
+fn add(input: From<AddInput>) -> String {
+    format!("Answer is: {}",  data)
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, print_val, add])
 }
