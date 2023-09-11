17e546d445999c044a844c830f076d0bdafb7d9f
minkyu.kim <thechaos16@gmail.com>
2023-09-11 22:17:25 +0900

    update post for rocket

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rocket::form::Form;
-
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+use rocket_contrib::json::Json;
+use serde::Deserialize;
 
-#[derive(FromForm)]
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
 struct AddInput {
     a: i32,
     b: i32
@@ -19,12 +21,11 @@ fn print_val(string: String) -> String {
 }
 
 #[post("/add", data = "<input>")]
-fn add(input: Form<AddInput>) -> String {
-    format!("Answer is: {}",  "abc")
+fn add(input: Json<AddInput>) -> String {
+    format!("Answer is: {}",  input.a + input.b)
 }
 
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, print_val, add])
+fn main() {
+    rocket::ignite().mount("/", routes![index, print_val, add]).launch();
 }
